4a6470d524226f6c5df6672f11abcadbcaf7bb1d
Nickolas Ribeiro <nickolas.santos@fortics.com.br>
2025-03-02 18:30:00 -0300

    feat(): forgot password

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -1,29 +1,45 @@
-<script setup lang="ts">
+<script lang="ts">
+import PrimaryButton from '@/components/buttons/PrimaryButton.vue';
 import InputError from '@/components/InputError.vue';
 import TextLink from '@/components/TextLink.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Input as UIInput } from '@/components/ui/input';
+import { Label as UILabel } from '@/components/ui/label';
 import AuthLayout from '@/layouts/AuthLayout.vue';
-import { Head, useForm } from '@inertiajs/vue3';
-import { LoaderCircle } from 'lucide-vue-next';
-
-defineProps<{
-    status?: string;
-}>();
-
-const form = useForm({
-    email: '',
-});
-
-const submit = () => {
-    form.post(route('password.email'));
+import { Head as InertiaHead, useForm } from '@inertiajs/vue3';
+export default {
+    components: {
+        AuthLayout,
+        InertiaHead,
+        InputError,
+        TextLink,
+        UIInput,
+        UILabel,
+        PrimaryButton
+    },
+    props: {
+        status: {
+            type: String,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            form: useForm({
+                email: '' as string | number,
+            }),
+        };
+    },
+    methods: {
+        submit() {
+            this.form.post(route('password.email'));
+        },
+    },
 };
 </script>
 
 <template>
     <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-        <Head title="Forgot password" />
+        <InertiaHead title="Forgot password" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -32,22 +48,19 @@ const submit = () => {
         <div class="space-y-6">
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
+                    <UILabel for="email">email address</UILabel>
+                    <UIInput id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
                     <InputError :message="form.errors.email" />
                 </div>
 
                 <div class="my-6 flex items-center justify-start">
-                    <Button class="w-full" :disabled="form.processing">
-                        <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                        Email password reset link
-                    </Button>
+                    <PrimaryButton :disabled="form.processing" :loading="form.processing" label="email password reset link" :class="'w-full'"/>
                 </div>
             </form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
-                <span>Or, return to</span>
-                <TextLink :href="route('login')">log in</TextLink>
+                <span>or, return to</span>
+                <TextLink :href="route('login', { loginState: true })">log in</TextLink>
             </div>
         </div>
     </AuthLayout>
