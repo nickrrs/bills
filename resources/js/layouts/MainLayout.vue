b890ea176bacd0ae199d4c577fe352b5cfbb0fbf
Nickolas Ribeiro <nickolas.santos@fortics.com.br>
2025-03-08 16:33:27 -0300

    feat(front): app main layout

---
+++ resources/js/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-screen relative h-screen flex flex-col bg-[#0D0D0D]">
+        <MainNavbar />
+        <div class="w-full h-full overflow-auto">
+            <slot />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import MainNavbar from '@/components/navbar/MainNavbar.vue';
+
+export default {
+    components: {
+        MainNavbar,
+    },
+};
+</script>
