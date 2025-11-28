afd62fb5d21c9bbf4399d283357390bc9fba813d
Nickolas Ribeiro <nickolasleosantos@hotmail.com>
2025-11-28 12:42:12 -0300

    feat(front): apply responsiveness to main navbar

--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -1,20 +1,24 @@
 <template>
-    <div class="w-screen relative h-screen flex flex-col bg-background">
-        <Toaster />
-        <MainNavbar :no-sub-nav="noSubNav" />
-        <div class="w-full h-full overflow-auto">
-            <slot />
+    <TooltipProvider>
+        <div class="w-screen relative h-screen flex flex-col bg-background">
+            <Toaster />
+            <MainNavbar :no-sub-nav="noSubNav" />
+            <div class="w-full h-full overflow-auto">
+                <slot />
+            </div>
         </div>
-    </div>
+    </TooltipProvider>
 </template>
 <script lang="ts">
 import MainNavbar from '@/components/navbar/MainNavbar.vue';
 import { Toaster } from '@/components/ui/toast';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 export default {
     components: {
         MainNavbar,
         Toaster,
+        TooltipProvider,
     },
     props: {
         noSubNav: {
